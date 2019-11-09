e8ce32294ab5ac922d4c002bc43da1bccfe86612
wk <eheroeric5459@163.com>
2019-11-09 17:37:53 +0800

    创建了login盒子模型

--- firstvue/src/main.js
+++ firstvue/src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 全局引入css样式
+import '@/assets/css/global.css'
 
 Vue.config.productionTip = false
 
--- firstvue/src/views/Login.vue
+++ firstvue/src/views/Login.vue
@@ -1,17 +1,26 @@
 <template>
   <div class="login-container">
-      <div class="login-box">
-          <p>展示</p>
-      </div>
+    <div class="login-box">
+      <p>展示</p>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
+.login-container {
+  height: 100%;
+  background-color: gray;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.login-box {
+  width: 410px;
+  height: 345px;
+  background-color: #fff;
+}
 </style>
