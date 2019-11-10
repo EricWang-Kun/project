9464f46eb980fa7e472033abddbbc3a22588ab21
wk <eheroeric5459@163.com>
2019-11-10 15:27:51 +0800

    安装gt.js，添加强制登录

--- firstvue/src/router/index.js
+++ firstvue/src/router/index.js
@@ -7,12 +7,20 @@ const routes = [
   // @ 是系统给封装好的别名，代表src目录的"绝对路径名"信息
   // @：E:\Vue86-87\86\86everyday\02daypro\topline\src\
   { path: '/login', name: 'login', component: () => import('@/views/Login') },
-  { path: '/home', name: 'home', component: () => import('@/views/Home') },
+  { path: '/home', name: 'home', component: () => import('@/views/Home') }
   // { path: '/', name: 'home', component: () => import('@/views/Login') }
 ]
-
 const router = new VueRouter({
   routes
+})
+router.beforeEach((to, from, next) => {
+  // 用户如果是"非登录"状态，并且其还要访问 非登录 页面，那么就强制登录去
+  var userinfo = window.sessionStorage.getItem('userinfo')
+  if (!userinfo && to.path !== '/login') {
+    // 路由导航到登录页面
+    return next('/login') // ok
+  }
+  next()
 })
 
 export default router
